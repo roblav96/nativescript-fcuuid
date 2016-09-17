6ac780bb34d1a8cf3c667a91af38b02e36e5c9ae
Ignacio Larrañaga <ignaciolarranaga@gmail.com>
2016-09-17 10:10:50 -0300

    Just adding examples.

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,10 +1,11 @@
 import {Observable} from 'data/observable';
+declare var FCUUID;
 
 export class HelloWorldModel extends Observable {
 	public message: string;
 
 	constructor() {
 		super();
-		this.message = 'FCUUID';
+		this.message = 'FCUUID: ' + FCUUID.uuidForDevice();
 	}
 }
